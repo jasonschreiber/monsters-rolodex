741374c6575ab8cf4592c1e4c9464f1e85737bf3
jschreiber <jschreiber@esri-southafrica.com>
2020-02-27 16:43:11 +0200

    Updates

---
+++ precache-manifest.55863b4954b559823d745d1d9a65bc74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e265bbc62055a2133abb558003d7c6b7",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "629a83759b5b7488ac2d",
+    "url": "/monsters-rolodex/static/css/main.678d5074.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "629a83759b5b7488ac2d",
+    "url": "/monsters-rolodex/static/js/main.18817b73.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
